de5d1129e09252d1a8f238ca30f7b85eb79fb6e0
gary-home <gary64800@gmail.com>
2021-08-16 21:11:10 +0800

    modify read dir.

--- test_read_dir.cpp
+++ test_read_dir.cpp
@@ -1,11 +1,13 @@
 #include <string>
 #include <iostream>
+#include <filesystem>
 #include <experimental/filesystem>
+
 namespace fs = std::experimental::filesystem;
 
 int main()
 {
-    std::string path = "/book/";
+    std::string path = "/path/to/directory";
     for (const auto & entry : fs::directory_iterator(path))
         std::cout << entry.path() << std::endl;
 }
